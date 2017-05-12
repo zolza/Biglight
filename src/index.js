1dca71ddd5eccfa5cde37dad8203e8a72ebcc1f5
zolza <kinga.koziol6@gmail.com>
2017-05-12 16:57:59 +0200

    Further version of animation.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 var catalogContainer = document.getElementById('catalog-container');
 var productListing = document.getElementById('product-listing');
 var filterMenu = document.getElementById('search-results-header-container');
-var buttonClickable = document.getElementsByClassName('filter-search-btn-js')[0];
 var filterOptions = document.getElementById('nav-dept');
+var buttonClickable = document.getElementsByClassName('filter-search-btn-js')[0];
+// var buttonCloseFilter = Array.prototype.slice.call(document.getElementsByClassName('apply-filter-js'));
 var content = document.getElementById('page-contents');
 var scrollTop = window.scrollY;
+
+function addElements() {
+  var textContainer = document.createElement('div');
+  textContainer.setAttribute('id', 'title-inject');
+  var text = document.createTextNode('FILTER');
+  textContainer.appendChild(text);
+
+  if (!document.querySelector('title-inject')) {
+     filterOptions.append(textContainer);
+  };
+}
 
 function scrollHandler() {
   var rectProductListing = productListing.getBoundingClientRect();
@@ -20,23 +32,26 @@ function scrollHandler() {
     rectProductListing.bottom < 635);
 };
 
-function clickHandler() {
-  // I can not make it work, but I decided to leave it here so you know that at least I have tried.
-
-  // var textContainer = document.createElement('div');
-  // textContainer.setAttribute('id', 'title-inject');
-  // var text = document.createTextNode('FILTER');
-  // textContainer.appendChild(text);
-  //
-  // if (!document.querySelector('title-inject')) {
-	//    filterOptions.append(textContainer);
-  // };
-
+function clickHandlerShowFilter() {
+  var scrollToptest = window.scrollY;
+  filterOptions.style.display = 'none';
   filterOptions.classList.add('nav-dept-inject');
+  filterOptions.style.top = scrollToptest - 260 + 'px';
 };
 
+// function clickHandlerHideFilter() {
+//   console.log('ej');
+//   // filterMenu.style.display = 'block';
+//   // filterOptions.style.display = 'none';
+// };
+
+
 function animate() {
-  buttonClickable.addEventListener('click', clickHandler);
+  addElements();
+  buttonClickable.addEventListener('click', clickHandlerShowFilter);
+  // buttonCloseFilter.forEach(function(item){
+  //   item.addEventListener('click', clickHandlerShowFilter)
+  // });
   document.addEventListener('scroll', scrollHandler);
 };
 
